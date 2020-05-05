7a658a4f0cd9d6c32d8288ac77edc4ed5348d1b0
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-05-04 22:15:29 -0300

    Updates

---
+++ precache-manifest.728d6bb7c303648f03380fc54253d7c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7580904cd06d6070036f34a46da65743",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "59bf3873c4ef76eae7ff",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/portfolio/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bf3873c4ef76eae7ff",
+    "url": "/portfolio/static/js/main.b4de061b.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
