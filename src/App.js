5cd7471e33c92f594e88563021fdb36f5a6a3c2a
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-11-18 12:05:53 -0300

    Added loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './normalize.css';
+import Loading from './components/Loading/Loading';
 import Navbar from './components/Navbar/Navbar';
 import LanguagesBar from './components/LanguagesBar/LanguagesBar';
 import Projects from './components/Projects/Projects';
@@ -11,19 +12,30 @@ import ReactGA from 'react-ga';
 const trackingId = "G-9HTSJYY6CT";
 ReactGA.initialize(trackingId);
 
-const App = () => (
-  <div className="App">
-    <Navbar />
-    <div className="main">
-      <LanguagesBar />
-      <hr />
-      <Projects />
-      <hr />
-      <Experiences />
-      <hr />
-      <Courses />
-    </div>
-  </div>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  return (
+      <div className="App">
+        {loading ? <Loading /> : (
+        <>
+          <Navbar />
+          <div className="main">
+            <LanguagesBar />
+            <hr />
+            <Projects />
+            <hr />
+            <Experiences />
+            <hr />
+            <Courses />
+          </div>
+        </>
+        ) }
+      </div>
+  );
+}
 
 export default App;
