cf804dfe4aafc9fec33b3e4e12a45fb9b3bd334a
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2021-02-09 10:59:49 -0300

    Updated personal info

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,37 +4,40 @@ import './Card.css';
 
 const Card = props => {
   const formatDescription = description => {
-    return description.substr(0, 148) + '...';
+    if (description) {
+      return description.substr(0, 148) + '...';
+    }
+    return description;
   };
-  const Info = ({info}) => {
-    return (info) ?  <p>{info}</p> : false;
-  }
-  const AdditionalInfo = ({info}) => {
-    return (info) ?  <p>{info}</p> : false;
-  }
-  const SubInfo = ({info}) => {
-    return (info) ?  <p>{info}</p> : false;
-  }
+  const Info = ({ info }) => {
+    return info ? <p>{info}</p> : false;
+  };
+  const AdditionalInfo = ({ info }) => {
+    return info ? <p>{info}</p> : false;
+  };
+  const SubInfo = ({ info }) => {
+    return info ? <p>{info}</p> : false;
+  };
   return (
-  <div
-    className={`card ${props.color}`}
-    onClick={() => window.open(props.link, '_blank')}
-  >
-    <div className="container card-content">
-      <div className="title">
-        <h4>{props.title}</h4>
-      </div>
-      <div className="info">
-        {props.children}
-        <Info info={props.info} />
-        <AdditionalInfo info={props.additionalInfo} />
-        <SubInfo info={props.subInfo} />
-        <div className="description">
-          <span>{formatDescription(props.description)}</span>
+    <div
+      className={`card ${props.color}`}
+      onClick={() => window.open(props.link, '_blank')}
+    >
+      <div className="container card-content">
+        <div className="title">
+          <h4>{props.title}</h4>
+        </div>
+        <div className="info">
+          {props.children}
+          <Info info={props.info} />
+          <AdditionalInfo info={props.additionalInfo} />
+          <SubInfo info={props.subInfo} />
+          <div className="description">
+            <span>{formatDescription(props.description)}</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
